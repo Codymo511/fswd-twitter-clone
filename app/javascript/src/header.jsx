2eb44078274681adfdaa3b8e032281cb64148764
Codymo511 <codymo511@gmail.com>
2023-03-17 07:56:41 -0400

    added header

---
+++ app/javascript/src/header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Header = props => (
+    <h1>Header is working</h1>
+  )
+
+  export default Header
--- app/javascript/src/home.jsx
+++ app/javascript/src/home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Header from './header';
 
 import './home.scss';
+
+
 
 const Home = props => (
   <h1>Home page react is working</h1>
 )
 
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
+    <Header />,
     <Home />,
     document.body.appendChild(document.createElement('div')),
   )
