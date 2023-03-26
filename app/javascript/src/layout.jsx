dd3e219705f56914339f872191df2b0bcd1aade8
Codymo511 <codymo511@gmail.com>
2023-03-26 15:20:13 -0400

    changed title

--- app/javascript/src/layout.jsx
+++ app/javascript/src/layout.jsx
@@ -10,7 +10,7 @@ const Layout = (props) => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto">
               <li className="nav-item">
-                <a className="nav-link" href="/">Home</a>
+               
               </li>
             </ul>
           </div>
