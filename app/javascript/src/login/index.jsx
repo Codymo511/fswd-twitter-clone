af8eca280a6caf7be3ba94d39d6d90d616436acf
Codymo511 <codymo511@gmail.com>
2023-05-01 17:49:15 -0400

    fixed error

--- app/javascript/src/login/index.jsx
+++ app/javascript/src/login/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './loginPage';
-import Signup from './signupWidget';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Login />,
-    <Signup />,
     document.body.appendChild(document.createElement('div')),
   )
 })
