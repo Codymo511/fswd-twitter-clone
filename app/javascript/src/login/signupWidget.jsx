6d8da95c156895b7b3a20c5bb08bbcaa4ccd5312
Codymo511 <codymo511@gmail.com>
2023-05-01 15:32:45 -0400

    add package

--- app/javascript/src/login/signupWidget.jsx
+++ app/javascript/src/login/signupWidget.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { safeCredentials, handleErrors } from '@utils/fetchHelper';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class Signup extends React.Component {
     
