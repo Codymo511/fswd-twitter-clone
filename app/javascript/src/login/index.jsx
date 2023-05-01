8efcdfee47c62404e2d1a5d55c1d84a9497e5125
Codymo511 <codymo511@gmail.com>
2023-05-01 17:46:49 -0400

    added signup

--- app/javascript/src/login/index.jsx
+++ app/javascript/src/login/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Login from './loginPage';
+import Signup from './signupWidget';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Login />,
+    <Signup />,
     document.body.appendChild(document.createElement('div')),
   )
 })
